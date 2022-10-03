68e99d794c938ba6bda4e59b221bbf9d77d48f0e
susmonteiro <s.moreno.monteiro@tecnico.ulisboa.pt>
2022-10-03 18:08:15 +0100

    Compare result of expression with INT_MAX is not a good solution

--- clang/lib/StaticAnalyzer/Checkers/SimpleOverflowChecker.cpp
+++ clang/lib/StaticAnalyzer/Checkers/SimpleOverflowChecker.cpp
@@ -4,6 +4,7 @@
 #include "clang/StaticAnalyzer/Core/Checker.h"
 #include "clang/StaticAnalyzer/Core/CheckerManager.h"
 #include "clang/StaticAnalyzer/Core/PathSensitive/CheckerContext.h"
+#include <limits.h>
 
 // TODO remove following import
 #include <iostream>
@@ -24,24 +25,25 @@ class SimpleOverflowChecker : public Checker<check::PreStmt<BinaryOperator>> {
 } // end anonymous namespace
 
 // TODO change this
-static const Expr *getDenomExpr(const ExplodedNode *N) {
+static const Expr *getExpr(const ExplodedNode *N) {
   const Stmt *S = N->getLocationAs<PreStmt>()->getStmt();
   if (const auto *BE = dyn_cast<BinaryOperator>(S))
     return BE->getRHS();
   return nullptr;
 }
 
+
+
 void SimpleOverflowChecker::reportBug(
     const char *Msg, ProgramStateRef StateZero, CheckerContext &C,
     std::unique_ptr<BugReporterVisitor> Visitor) const {
   if (ExplodedNode *N = C.generateErrorNode(StateZero)) {
     if (!BT)
-    // TODO remove xxx
-      BT.reset(new BuiltinBug(this, "Overflow xxx"));
+      BT.reset(new BuiltinBug(this, "Overflow"));
 
     auto R = std::make_unique<PathSensitiveBugReport>(*BT, Msg, N);
     R->addVisitor(std::move(Visitor));
-    bugreporter::trackExpressionValue(N, getDenomExpr(N), *R);
+    bugreporter::trackExpressionValue(N, getExpr(N), *R);
     C.emitReport(std::move(R));
   }
 }
@@ -49,42 +51,37 @@ void SimpleOverflowChecker::reportBug(
 void SimpleOverflowChecker::checkPreStmt(const BinaryOperator *B,
                                          CheckerContext &C) const {
   BinaryOperator::Opcode Op = B->getOpcode();
-  // if (Op != BO_Div && Op != BO_Rem && Op != BO_DivAssign && Op != BO_RemAssign)
-  //   return;
 
-  // if (!B->getRHS()->getType()->isScalarType())
-  //   return;
-
-  if (Op != BO_Add) 
+  if (Op != BO_Add && Op != BO_AddAssign && 
+      Op != BO_Sub && Op != BO_SubAssign && 
+      Op != BO_Mul && Op != BO_MulAssign &&
+      Op != BO_Div && Op != BO_DivAssign) 
     return;
+  
+  if (!B->getRHS()->getType()->isScalarType())
+    return;
+
+
+  // Check if result of a Op b < INT_MAX
+  ConstraintManager &CM = C.getConstraintManager();
+  ProgramStateRef stateNotZero, stateZero;
 
   SVal left = C.getSVal(B->getLHS());
   SVal right = C.getSVal(B->getRHS());
-  Optional<DefinedSVal> DVL = left.getAs<DefinedSVal>();
-  Optional<DefinedSVal> DVR = right.getAs<DefinedSVal>();
-
-  std::cout << "Print please?" << std::endl;
-
-  // If one of the values is undefined, don't report Bug
-  if (!DVL || !DVR)
-    return;
-  
-
-  // Check if a + b == 0
-  ConstraintManager &CM = C.getConstraintManager();
-  ProgramStateRef stateNotZero, stateZero;
   
   SValBuilder &SVB = C.getSValBuilder();
-  SVal EqualZero = SVB.evalBinOp(C.getState(), BO_Add, left, right, SVB.getConditionType());
+  SVal applyOperation = SVB.evalBinOp(C.getState(), Op, left, right, SVB.getConditionType());
 
-  if (Optional<DefinedSVal> EqualZeroDVal =
-          EqualZero.getAs<DefinedSVal>()) {
+  SVal isOverflow = SVB.evalBinOp(C.getState(), BO_LT, applyOperation, SVB.makeIntVal(INT_MAX, false), SVB.getConditionType());  
+
+  if (Optional<DefinedSVal> isOverflowDVal =
+          isOverflow.getAs<DefinedSVal>()) {
     // Returns a pair of states (StInRange, StOutOfRange) where the given value is assumed to be in the range or out of the range, respectively.
-    std::tie(stateNotZero, stateZero) = CM.assumeDual(C.getState(), *EqualZeroDVal);
+    std::tie(stateNotZero, stateZero) = CM.assumeDual(C.getState(), *isOverflowDVal);
 
     if (!stateNotZero) {
       assert(stateZero);
-      reportBug("Add is forbidden", stateZero, C);
+      reportBug("Found overflow", stateZero, C);
       return;
     }
   }
