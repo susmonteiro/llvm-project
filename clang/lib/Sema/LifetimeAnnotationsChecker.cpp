4d3e419e5c5a0a6c93f7ebb1261edd028681bd3b
susmonteiro <s.moreno.monteiro@tecnico.ulisboa.pt>
2023-04-04 17:57:58 +0100

    Create checker in Sema * make Sema call LifetimeAnnotationsChecker debug function * very simple

---
+++ clang/lib/Sema/LifetimeAnnotationsChecker.cpp
@@ -0,0 +1,7 @@
+#include "LifetimeAnnotationsChecker.h"
+
+#include <iostream>
+
+void LifetimeAnnotationsChecker::debug(std::string txt) {
+  std::cout << txt << '\n';
+}
---
+++ clang/lib/Sema/LifetimeAnnotationsChecker.h
@@ -0,0 +1,6 @@
+#include <string>
+
+class LifetimeAnnotationsChecker {
+ public:
+  void debug(std::string txt);
+};
