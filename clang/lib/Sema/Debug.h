83cc6c42f8cd4c44bad157ea662d2e143bac42de
susmonteiro <s.moreno.monteiro@tecnico.ulisboa.pt>
2023-04-07 00:10:52 +0100

    Create file with debug functions

---
+++ clang/lib/Sema/Debug.h
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+
+#include "llvm/ADT/SmallVector.h"
+
+// TODO delete this file
+
+void debug(std::string txt) { std::cout << txt << '\n'; }
+void debug(std::string txt1, std::string txt2) { std::cout << txt1 << ": " << txt2 << '\n'; }
