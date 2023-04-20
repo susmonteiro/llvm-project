1652b5f8d0ef2460b8895cb581d5f99b39e52d0e
susmonteiro <s.moreno.monteiro@tecnico.ulisboa.pt>
2023-04-20 15:23:45 +0100

    Create light debug

--- clang/include/clang/Sema/DebugLifetimes.h
+++ clang/include/clang/Sema/DebugLifetimes.h
@@ -25,5 +25,6 @@ void debugLifetimes(llvm::DenseMap<const clang::NamedDecl *, llvm::DenseSet<cons
 void debugInfo(std::string txt);
 void debugInfo(std::string txt, int i);
 void debugWarn(std::string txt);
+void debugLight(std::string txt);
 
 #endif  // LIFETIME_ANNOTATIONS_DEBUG_H_
