0765ed105711d5763b5ec7adf22cc5ee24054bf6
susmonteiro <s.moreno.monteiro@tecnico.ulisboa.pt>
2023-05-21 13:51:36 +0100

    Silence debug messages

--- clang/include/clang/Sema/DebugLifetimes.h
+++ clang/include/clang/Sema/DebugLifetimes.h
@@ -7,27 +7,35 @@
 #include "clang/AST/Attr.h"
 #include "clang/AST/Attrs.inc"
 #include "clang/AST/DeclCXX.h"
-#include "llvm/ADT/SmallVector.h"
 #include "llvm/ADT/DenseSet.h"
+#include "llvm/ADT/SmallVector.h"
 
 // TODO delete this file
 
 void debugLifetimes(std::string txt);
 void debugLifetimes(std::string txt1, std::string txt2);
 void debugLifetimes(std::string txt, int i);
-void debugLifetimes(llvm::SmallVector<const clang::Expr*> vec);
-void debugLifetimes(llvm::SmallVector<const clang::Attr*> vec);
+void debugLifetimes(llvm::SmallVector<const clang::Expr *> vec);
+void debugLifetimes(llvm::SmallVector<const clang::Attr *> vec);
 void debugLifetimes(llvm::SmallVector<std::string> vec);
-void debugLifetimes(std::vector<const clang::NamedDecl*> vec);
-void debugLifetimes(llvm::DenseSet<const clang::NamedDecl*> vec);
+void debugLifetimes(std::vector<const clang::NamedDecl *> vec);
+void debugLifetimes(llvm::DenseSet<const clang::NamedDecl *> vec);
 void debugLifetimes(llvm::DenseSet<char> vec);
-void debugLifetimes(llvm::DenseMap<const clang::NamedDecl *, llvm::DenseSet<const clang::NamedDecl *>> m);
+void debugLifetimes(llvm::DenseMap<const clang::NamedDecl *,
+                                   llvm::DenseSet<const clang::NamedDecl *>>
+                        m);
+void debugLifetimes(llvm::DenseMap<const clang::NamedDecl *,
+                                   llvm::DenseSet<const clang::Stmt *>>
+                        var_stmt,
+                    llvm::DenseMap<const clang::Stmt *,
+                                   llvm::DenseSet<const clang::NamedDecl *>>
+                        stmt_var);
 
 void debugImportant(std::string txt);
 void debugImportant(std::string txt1, std::string txt2);
 void debugInfo(std::string txt);
-void debugInfo2(std::string txt);
 void debugInfo(std::string txt, int i);
+void debugInfo2(std::string txt);
 void debugWarn(std::string txt);
 void debugWarn2(std::string txt);
 void debugLight(std::string txt);
